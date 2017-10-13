eb4fbfb4a8f149e6a68b73a85d6286dc3df36c6d
Anna Ingels <annaingels@annas-mbp.attlocal.net>
2017-10-13 12:15:18 -0700

    created main.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,16 @@
 import { NGModule } from '@angular/core';
+import { BrowserModule  } from '@agular/platform-browser';
+import { AppComponent } from '.app/component';
 
-@NGModule()
+@NGModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
 export class AppModule {};
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic }  from '@angular/platform-bowser-dynamic'; 
+import { AppModule } from '/app.module'; 
+
+platformBrowserDynamic().bootstrapModule(AppModule); 
