78cd7d1b4186d57bc3708fdca8cde61fb8cc4726
Anna Ingels <annaingels@Annas-MacBook-Pro.local>
2017-10-18 15:48:56 -0700

    tutorial

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { MediaItemComponent } from './media-item.component'
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent,
   ],
   bootstrap: [
-    AppComponent
+    AppComponent,
   ]
 })
 export class AppModule {}
